4196227e6076180f659bb608da6cd9d4e51a2e81
nhn09 <68825961+nhn09@users.noreply.github.com>
2020-07-27 09:08:06 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,53 @@
 #include"Person.h"
 #include"Doner.h"
 #include"Functions.h"
+#include<iostream>
+#include<thread>
+#include<chrono>
+#include<windows.h>
+
 using namespace std;
+
+
+using std::this_thread::sleep_for;
+using std::chrono::milliseconds;
+
+void print(const string&,unsigned int);
+
+void print(const string& messege, unsigned int mil)
+
+{
+    for(const char c:messege)
+    {
+        cout<< c << flush;
+        sleep_for(milliseconds(mil));
+    }
+}
+
+void gotoxy(int x,int y)
+{
+COORD c;
+c.X =x;
+c.Y=y;
+SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE) , c);
+}
+
 void startMenu()
 {
     Person *person[10000];
     char donerOrDoneeChoice;
    int personCount=0;
-   system("color b");
+   system("color b0");
    system("cls");
-   cout<<"\t\t\t\t\t***  Welcome to MANOBOTAR DEYAL  ***"<<endl;
-   cout<<"\n\nEnter as :"<<endl;
-   cout<<"a. Doner"<<endl;
+   gotoxy(60,10);
+   cout<<"  Welcome to MANOBOTAR DEYAL  "<<endl;
+   gotoxy(65,15);
+   cout<<"Enter as :"<<endl;
+   gotoxy(66,17);
+   cout<<"a.Donor"<<endl;
+   gotoxy(66,18);
    cout<<"b.Donee"<<endl;
+   gotoxy(65,22);
    cout<<"Enter your choice :";
    cin>>donerOrDoneeChoice;
    if(donerOrDoneeChoice=='a')
@@ -26,6 +61,11 @@ void startMenu()
 
 int main()
 {
+
+    string messege = " We cannot despair of humanity, since we ourselves are human beings. Albert Einstein.  ...";
+     system("color F0");
+     gotoxy(40,10);
+     print(messege,140);
     //Person person;
     char c;
 
